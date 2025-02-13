b706a9c9f8adfa347b8f8467bd5b6a81ba2dad3b
RedFox197 <190109902+RedFox197@users.noreply.github.com>
2025-02-13 12:56:18 +0100

    Initial Commit

---
+++ src/main/java/com/github/redfox197/bedandbreakfast/BedandbreakfastApplication.java
@@ -0,0 +1,13 @@
+package com.github.redfox197.bedandbreakfast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BedandbreakfastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BedandbreakfastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/redfox197/bedandbreakfast/BedandbreakfastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.redfox197.bedandbreakfast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BedandbreakfastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
